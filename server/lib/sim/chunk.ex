36670a27fc188e5e7ca932c50565f2a0dd0cac23
David Simon <david.mike.simon@gmail.com>
2016-12-06 21:47:19 -0500

    Proper block run generation in chunk, wip

--- server/lib/sim/chunk.ex
+++ server/lib/sim/chunk.ex
@@ -2,6 +2,62 @@ defmodule Werld.Sim.Chunk do
   use GenServer
 
   ####
+  ## Internals
+  ####
+
+  defmodule State do
+    defstruct [:block_types]
+  end
+
+  @chunk_size 16
+
+  defp initial_state do
+    block_types = :array.new([
+      size: @chunk_size*@chunk_size,
+      default: 0,
+      fixed: true
+    ])
+    block_types = :array.set(3, 1, block_types)
+    block_types = :array.set(4, 1, block_types)
+    block_types = :array.set(9, 1, block_types)
+
+    %Werld.Sim.Chunk.State{
+      block_types: block_types
+    }
+  end
+
+  defp to_chunk_proto(state) do
+    Werld.Proto.Chunk.new(
+      pos: Werld.Proto.Coord.new(
+        instance: 0,
+        grid: 0,
+        x: 0,
+        y: 0
+      ),
+      ver: 50,
+      block_runs: block_types_array_to_runs(state.block_types)
+    )
+  end
+
+  defp block_types_array_to_runs(arr, idx \\ 0) do
+    if idx > :array.size(arr) do
+      []
+    else
+      cur = :array.get(idx, arr)
+      case block_types_array_to_runs(arr, idx+1) do
+        [] ->
+          [Werld.Proto.Chunk.BlockRun.new(count: 1, block_type: cur)]
+        [next_run | rest] ->
+          if next_run.block_type == cur do
+            [Werld.Proto.Chunk.BlockRun.new(count: next_run.count+1, block_type: cur) | rest]
+          else
+            [Werld.Proto.Chunk.BlockRun.new(count: 1, block_type: cur) | [ next_run | rest ]]
+          end
+      end
+    end
+  end
+
+  ####
   ## Client API
   ####
   
@@ -19,7 +75,7 @@ defmodule Werld.Sim.Chunk do
 
   def init(:ok) do
     :gproc.reg({:n, :l, {:chunk, 0, 0, 0, 0}})
-    {:ok, %{}}
+    {:ok, initial_state}
   end
 
   def handle_call(:get_chunk_proto, _from, state) do
@@ -34,33 +90,4 @@ defmodule Werld.Sim.Chunk do
     {:noreply, state}
   end
 
-  ####
-  ## Internals
-  ####
-  
-  defp to_chunk_proto(state) do
-    Werld.Proto.Chunk.new(
-      pos: Werld.Proto.Coord.new(
-        instance: 0,
-        grid: 0,
-        x: 0,
-        y: 0 
-      ),
-      ver: 50,
-      block_runs: [
-        Werld.Proto.Chunk.BlockRun.new(
-          count: 20,
-          block_type: 0
-        ),
-        Werld.Proto.Chunk.BlockRun.new(
-          count: 50,
-          block_type: 1
-        ),
-        Werld.Proto.Chunk.BlockRun.new(
-          count: 186,
-          block_type: 0
-        ),
-      ]
-    )
-  end
 end
